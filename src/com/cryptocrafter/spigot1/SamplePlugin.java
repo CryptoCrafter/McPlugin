0976f40489d0fb4cdd8693d99c59032ba4ec8d68
CryptoCrafter <gdclark03@gmail.com>
2021-02-04 10:03:20 -0500

    Initial Commit.

---
+++ src/com/cryptocrafter/spigot1/SamplePlugin.java
@@ -0,0 +1,16 @@
+package com.cryptocrafter.spigot1;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SamplePlugin extends JavaPlugin{
+	
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+
+}
