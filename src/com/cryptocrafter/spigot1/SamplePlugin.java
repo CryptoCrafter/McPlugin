37423bff982a97022035c37ddf873306deaac0a7
BuildTools <unconfigured@null.spigotmc.org>
2021-02-05 09:43:04 -0500

    2/5/2021 - 2

--- src/com/cryptocrafter/spigot1/SamplePlugin.java
+++ src/com/cryptocrafter/spigot1/SamplePlugin.java
@@ -2,9 +2,7 @@ package com.cryptocrafter.spigot1;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerShearEntityEvent;
-import org.bukkit.metadata.Metadatable;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.messaging.PluginMessageRecipient;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
