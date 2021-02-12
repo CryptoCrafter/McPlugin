e46506ce2f2b8d8d788735c489dc2022e76f1361
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 11:06:17 -0500

    c

--- src/com/cryptocrafter/spigot1/commands/playertest.java
+++ src/com/cryptocrafter/spigot1/commands/playertest.java
@@ -1,5 +1,24 @@
 package com.cryptocrafter.spigot1.commands;
 
-public class playertest {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+public class playertest implements CommandExecutor {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("test")) {
+        	Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Test command has been executed!");
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage(ChatColor.AQUA + "Test Successful!" + player.getName() + "!");
+                return true;
 }
+            if(!(sender instanceof Player)) {
+            	sender.sendMessage("You must be a player to run this command");
+            }
+            }
+        return false;}
+	}
