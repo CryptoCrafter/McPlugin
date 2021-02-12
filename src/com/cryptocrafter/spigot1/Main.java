c1a2c6427290597d91e450b5f1867e3213bc491f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 00:25:38 -0500

    Complete changfe

--- src/com/cryptocrafter/spigot1/Main.java
+++ src/com/cryptocrafter/spigot1/Main.java
@@ -3,9 +3,10 @@ package com.cryptocrafter.spigot1;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable(){
