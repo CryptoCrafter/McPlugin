848ff1a86542f9b9684d231bd80d5a6d91b6511c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 09:14:00 -0500

    test

--- src/com/cryptocrafter/spigot1/listener/ChargedCreeper.java
+++ src/com/cryptocrafter/spigot1/listener/ChargedCreeper.java
@@ -4,13 +4,14 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
 
 public class ChargedCreeper {
-	//Set up a  location for possible spawn
+	//Sets up variables
 	double min;
 	double max;
 	World world;
@@ -19,7 +20,7 @@ public class ChargedCreeper {
 	double z;
 	Location location;
 	Location blockbellowcreeper;
-	
+	//Constructor
 	public ChargedCreeper() {
 		 min = 0.0;
 		 max = 10000;
@@ -33,7 +34,7 @@ public class ChargedCreeper {
 		//Checks if lighting spawn conditions are met
 	    if(blockbellowcreeper != null){
 	        int lightlevel = blockbellowcreeper.getBlock().getLightLevel();
-	        if(lightlevel <= 6) {
+	        if(lightlevel <= 9) {
 	        	//Does a random check for chance of spawn
 	        	int randmin = 0;
 	        	int randmax = 5;
@@ -41,6 +42,7 @@ public class ChargedCreeper {
 	            int rand1 = rand.nextInt((randmax - randmin) + 1) + randmin;
 	            if(rand1 == 4){
 	            	//Spawns mob
+	            	Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Creeper!");
 	        	Creeper creeper = (Creeper)world.spawnEntity(location, EntityType.CREEPER);
 	        	creeper.setPowered(true);
 	        	return;
