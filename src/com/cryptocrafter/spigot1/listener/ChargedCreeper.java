7786a3fa23ea65930f8e281ce6ac55735589e5d2
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 09:41:44 -0500

    bgbg

--- src/com/cryptocrafter/spigot1/listener/ChargedCreeper.java
+++ src/com/cryptocrafter/spigot1/listener/ChargedCreeper.java
@@ -1,24 +1,17 @@
 package com.cryptocrafter.spigot1.listener;
 
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 public class ChargedCreeper implements Listener{
-@EventHandler
 public void onEnable() {
-	Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Creeoer Spawn Enabled.!");
+	Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Creeper Spawn Enabled.!");
 }
-
+@EventHandler
    public void onMobSpawn(CreatureSpawnEvent e) {
 	   if(e.getEntityType() == EntityType.CREEPER) {
 		   e.setCancelled(true);
