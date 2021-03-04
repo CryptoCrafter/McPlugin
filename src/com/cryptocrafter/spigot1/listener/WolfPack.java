6e947a6de5141384520b72a1c1d6a8e77a9c7906
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 10:09:17 -0500

    fe

--- src/com/cryptocrafter/spigot1/listener/WolfPack.java
+++ src/com/cryptocrafter/spigot1/listener/WolfPack.java
@@ -3,6 +3,7 @@ package com.cryptocrafter.spigot1.listener;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
@@ -16,15 +17,17 @@ public class WolfPack implements Listener {
 		   if(W.getEntityType() == EntityType.WOLF) {
 			   //Wolf wolf = (Wolf) e.getEntity();
 			   //Wolf wolf = (Wolf) e.getEntity().getWorld().spawnEntity(e.getEntity().getLocation(), EntityType.WOLF);
-			   W.setCancelled(true);
+			   Location loc = W.getLocation();
 			   Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Wolf Spawn cancel.!");
-			   for(int i = 0; i<6; i++) {
-				Location loc = W.getLocation();
+			   int randomInt = (int)(3.0 * Math.random());
+			   if(randomInt == 2) {
+			   for(int i = 0; i<6; i++) {;
+			   if(((Ageable) W).isAdult() == false) {
                Wolf wolf = (Wolf) W.getEntity().getWorld().spawnEntity(loc, EntityType.WOLF);
                wolf.setAngry(true);
                //W.setCancelled(true);
 			   Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Wolf Spawn Tester.!");
-			   }
+			   }}}
 			   }
 	   }
 	}
