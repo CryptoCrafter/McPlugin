a83ebf09dbb26a48bd8d3b6b1416599c7e747773
BuildTools <unconfigured@null.spigotmc.org>
2021-03-16 09:55:44 -0400

    Possible fix

--- src/com/cryptocrafter/spigot1/listener/WolfPack.java
+++ src/com/cryptocrafter/spigot1/listener/WolfPack.java
@@ -10,28 +10,34 @@ import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
+@SuppressWarnings("unused")
 public class WolfPack implements Listener {
 
 	@EventHandler
 	public void onMobSpawn(CreatureSpawnEvent e) {
-		   if(e.getEntityType() == EntityType.WOLF) {
-			   Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "IFSTATEMENTPASSED.!");
-			   Wolf wolf = (Wolf) e.getEntity();
-			   Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Wolf Spawn cancel.!");
-			   int randomInt = (int)(3.0 * Math.random());
-			   System.out.println("Random int: " + randomInt);
-			   if(randomInt == 1) {
-				   //Wolf wolf = (Wolf) e.getEntity().getWorld().spawnEntity(e.getEntity().getLocation(), EntityType.WOLF);
-				   World world = ((Entity) wolf).getWorld();
-				   Location loc = wolf.getLocation();
-				   //e.setCancelled(true);
-				   for(int i = 0; i<5; i++) {
-				   		Wolf madwolf = (Wolf) world.spawnEntity(loc, EntityType.WOLF);
-				   		madwolf.setAngry(true);
-				   		madwolf.setCustomName("Tester" + i);
-				   		Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Wolf Spawn Tester.!");
-			   }}
+		if(e.getSpawnReason() == SpawnReason.NATURAL) {
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "natrual");
+			if(e.getEntity().getType() == EntityType.WOLF) {
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "IFSTATEMENTPASSED.!");
+			//Wolf wolf = (Wolf) e.getEntity();
+			//Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Wolf Spawn cancel.!");
+			int randomInt = (int)(3.0 * Math.random());
+			System.out.println("Random int: " + randomInt);
+			if(randomInt == 1) {
+				//Wolf wolf = (Wolf) e.getEntity().getWorld().spawnEntity(e.getEntity().getLocation(), EntityType.WOLF);
+				World world = e.getEntity().getWorld();
+				Location loc = e.getLocation();
+				e.setCancelled(true);
+				for(int i = 0; i<5; i++) {
+					 //Location loc = e.getLocation();
+					Wolf wolf=(Wolf)world.spawnEntity(loc,EntityType.WOLF);
+					 //Wolf madwolf = (Wolf) world.spawnEntity(loc, EntityType.WOLF);
+				   	 wolf.setAngry(true);
+				   	 wolf.setCustomName("Tester" + i);
+				   	 Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Wolf Spawn Tester.!");
+			   }}}
 			   }
 	   }
 	}
